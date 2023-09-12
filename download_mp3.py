10e041ce103b9b08a12538fa1f9ccaa5db8492a4
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2023-09-12 08:37:31 +0530

    Added features

--- download_mp3.py
+++ download_mp3.py
@@ -13,7 +13,9 @@ def getUrls(playlist_url):
     return urls, playlist_name
 
 def getMP3fromLink(video_link, destination,playlist_name = ""):
+    print(f"Starting with {video_link}")
     youtube_vid = pytube.YouTube(video_link)
+    print(f"\tName:{youtube_vid.title}")
     video = youtube_vid.streams.filter(only_audio=True).first()
 
     out_file = video.download(output_path=destination)
@@ -21,6 +23,7 @@ def getMP3fromLink(video_link, destination,playlist_name = ""):
     base,ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file,new_file)
+    print(f"\tFile Name: {new_file}")
     track = music_tag.load_file(new_file)
     track['title'] = youtube_vid.title
     track['artist'] = youtube_vid.author.replace('VEVO', '')
@@ -40,9 +43,22 @@ def getMP4fromLink(video_link, destination):
     print('COMPLETED')
 
 if __name__ == '__main__' :
-    destination = sys.argv[1]
-    playlist_url = sys.argv[2]
-    urls, playlist_name = getUrls(playlist_url)
-    for url in urls:
-        getMP3fromLink(url, playlist_name)
-    print("Done")
+    try:
+        if len(sys.argv) >= 3:
+            destination = sys.argv[2]
+            playlist_url = sys.argv[1]
+        print(f"Destination is {destination}\nPlaysist Url: {playlist_url}")
+        urls, playlist_name = getUrls(playlist_url)
+        print(playlist_name)
+        print(len(urls))
+        for url in urls:
+            try:
+                getMP3fromLink(url, destination, playlist_name)
+            except Exception as e:
+                print(f"Error -> {e}")
+                continue
+        print("Done")
+    except KeyboardInterrupt:
+        exit
+    except Exception as e:
+        print("Errr -> ", {e})
--- mp3_link.py
+++ mp3_link.py
@@ -1,5 +1,6 @@
 from download_mp3 import *
 
+destination = input("Destination: ")
 while 1:
     link = input("Youtube Link: ")
-    getMP3fromLink(link)
+    getMP3fromLink(link, destination)
